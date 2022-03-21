c89ce57d4c35f3fa2aa7e29fb9912c770ded8ac1
Feiruz Khouri <feiruz25@hotmail.com>
2022-03-21 11:41:16 +0100

    to write variables

--- js/main.js
+++ js/main.js
@@ -6,14 +6,59 @@ form.classList.remove('collapsed');
 
 
 const list = document.querySelector('.js-list');
+// declaras las vai¡riables//
 
-const kittenOne = '<li class="card"> <article> <img class="card_img"src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"  alt="gatito" /><h3 class="card_title">Anastacio</h3> <h4 class="card_race">British Shorthair</h4> <p class="card_description">   Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! </p> </article> </li>';
+const kittenImage1= "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const kittenName1 = "Anastacio";
 
-const kittenTwo = '<li class="card"> <img class="card_img" src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" /> <h3 class="card_title">Fiona</h3> <h4 class="card_race">British Shorthair</h4> <p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! </p> </li>';
+const kittenRace1 = "British Shorthair";
 
-const kittenThree = '<li class="card"> <img class="card_img" src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg" alt="gatito" /> <h3 class="card_title">Cielo</h3> <h4 class="card_race">British Shorthair</h4> <p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!</p> </li>';
+const kittenDesc1 =
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+// LLAMO A LAS VARIABLES
+
+const kittenOne = `<li class="card"> <article> <img class="card_img"src=${ kittenImage1}  alt="gatito" /><h3 class="card_title">${ kittenName1}</h3> <h4 class="card_race">${kittenRace1 }</h4> <p class="card_description">${kittenDesc1} </p> </article> </li>`;
+
+//DECLARO LAS VARIABLES
+
+const kittenImage2= "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
+const kittenName2 = "Fiona";
+
+const kittenRace2 = "British Shorthair";
+
+const kittenDesc2 =
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+
+  // LLAMO A LAS VARIABLES
+
+const kittenTwo = `<li class="card"> <img class="card_img" src=${kittenImage2} alt="gatito" /> <h3 class="card_title"> ${kittenName2}</h3> <h4 class="card_race">
+${kittenRace2}</h4> <p class="card_description">
+${kittenDesc2 }
+</p> </li>`;
+// declaro las varibles
+
+const kittenImage3= "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
+;
+const kittenName3= "Cielo";
+
+const kittenRace3 = "British Shorthair";
+
+const kittenDesc3 =
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+
+// LLAMO A LAS VAriables
+
+const kittenThree = `<li class="card"> <img class="card_img" src=${kittenImage3} alt="gatito" /> <h3 class="card_title"> ${kittenName3}</h3> <h4 class="card_race">
+${kittenRace3}</h4> <p class="card_description">
+${kittenDesc3 }
+</p> </li>`;
+
 
 list.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+
+
+
 
 
 
